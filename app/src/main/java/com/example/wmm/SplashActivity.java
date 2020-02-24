8b5eac660cf3c89cb79efedcc7b9d5fafa036636
suresh-k <ksuresh.unique@gmail.com>
2020-02-24 13:22:12 +0530

    second

--- app/src/main/java/com/example/wmm/SplashActivity.java
+++ app/src/main/java/com/example/wmm/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
