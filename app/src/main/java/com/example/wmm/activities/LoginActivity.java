add955bbe988310b8d313a3efeb518e1b070c170
Anil <anilwesley94@gmail.com>
2020-02-24 15:34:16 +0530

    login

--- app/src/main/java/com/example/wmm/activities/LoginActivity.java
+++ app/src/main/java/com/example/wmm/activities/LoginActivity.java
@@ -1,16 +1,59 @@
 package com.example.wmm.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import com.example.wmm.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @BindView(R.id.imgLogo)
+    ImageView imgLogo;
+    @BindView(R.id.etUserName)
+    EditText etUserName;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+    @BindView(R.id.btnSignIn)
+    Button btnSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#1A1E23"));
+        }
+
+
+    }
+
+    @OnClick(R.id.btnSignIn)
+    public void onViewClicked() {
+
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 }
--- app/src/main/java/com/example/wmm/activities/SplashActivity.java
+++ app/src/main/java/com/example/wmm/activities/SplashActivity.java
@@ -1,26 +1,32 @@
-package com.example.wmm.activities;
+package com.example.wmm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-import com.example.wmm.R;
-
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#1A1E23"));
+        }
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
         },3000);
     }
 }
