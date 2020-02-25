decc97f98fbf75d06116287647b1e96351b84116
Anil <anilwesley94@gmail.com>
2020-02-25 13:21:03 +0530

    login

--- app/src/main/java/com/example/wmm/fragments/HomeFragment.java
+++ app/src/main/java/com/example/wmm/fragments/HomeFragment.java
@@ -1,14 +1,28 @@
 package com.example.wmm.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.wmm.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -17,6 +31,25 @@ import com.example.wmm.R;
 public class HomeFragment extends Fragment {
 
     View rootView;
+    @BindView(R.id.txtworks)
+    TextView txtworks;
+    @BindView(R.id.txtWorkCount)
+    TextView txtWorkCount;
+    @BindView(R.id.txtInProgress)
+    TextView txtInProgress;
+    @BindView(R.id.txtProgreeCount)
+    TextView txtProgreeCount;
+    @BindView(R.id.txtComplete)
+    TextView txtComplete;
+    @BindView(R.id.txtCompletedCount)
+    TextView txtCompletedCount;
+    @BindView(R.id.txtPercentage)
+    TextView txtPercentage;
+    @BindView(R.id.circular_progress)
+    ProgressBar circularProgress;
+    @BindView(R.id.chart)
+    BarChart chart;
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -27,11 +60,36 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+
+        circularProgress.setMax(50);
+        circularProgress.setProgress(20);
+        circularProgress.setProgress(0); // <--
+        circularProgress.setMax(20);
+        circularProgress.setProgress(20);
+        txtPercentage.setText("75%");
 
 
+        BarDataSet bardataset = new BarDataSet(getData(), "No Of Employee");
+        chart.animateY(5000);
+        BarData data = new BarData(bardataset);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+       // bardataset.setBarBorderWidth(10f);
+       // bardataset.setBarBorderColor(Color.parseColor("#FA9611"));
+        bardataset.setValueTextColor(Color.WHITE);
+        bardataset.setHighLightColor(Color.WHITE);
+        chart.setData(data);
 
-        return  view;
+        return view;
     }
 
-
+    private ArrayList getData(){
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry (1, 5));
+        entries.add(new BarEntry (3, 7));
+        entries.add(new BarEntry (5,3));
+        entries.add(new BarEntry (7,4));
+        entries.add(new BarEntry (9,1));
+        return entries;
+    }
 }
